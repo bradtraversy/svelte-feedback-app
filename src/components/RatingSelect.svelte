6f4c5a41b72d8b8b260d52bff2cc893264febde1
Olebogeng Mbedzi <olebogeng350@gmail.com>
2023-12-14 00:01:46 +0200

    Make ratings iterable

--- src/components/RatingSelect.svelte
+++ src/components/RatingSelect.svelte
@@ -12,47 +12,20 @@
 </script>
 
 <ul class="rating">
-  <li>
-    <input type="radio" id="num1" name="rating" value="1" on:change={onChange} checked={selected===1} />
-    <label for="num1">1</label>
-  </li>
-  <li>
-    <input type="radio" id="num2" name="rating" value="2" on:change={onChange} checked={selected===2} />
-    <label for="num2">2</label>
-  </li>
-  <li>
-    <input type="radio" id="num3" name="rating" value="3" on:change={onChange} checked={selected===3} />
-    <label for="num3">3</label>
-  </li>
-  <li>
-    <input type="radio" id="num4" name="rating" value="4" on:change={onChange} checked={selected===4} />
-    <label for="num4">4</label>
-  </li>
-  <li>
-    <input type="radio" id="num5" name="rating" value="5" on:change={onChange} checked={selected===5} />
-    <label for="num5">5</label>
-  </li>
-  <li>
-    <input type="radio" id="num6" name="rating" value="6" on:change={onChange} checked={selected===6} />
-    <label for="num6">6</label>
-  </li>
-  <li>
-    <input type="radio" id="num7" name="rating" value="7" on:change={onChange} checked={selected===7} />
-    <label for="num7">7</label>
-  </li>
-  <li>
-    <input type="radio" id="num8" name="rating" value="8" on:change={onChange} checked={selected===8} />
-    <label for="num8">8</label>
-  </li>
-  <li>
-    <input type="radio" id="num9" name="rating" value="9" on:change={onChange} checked={selected===9} />
-    <label for="num9">9</label>
-  </li>
-  <li>
-    <input type="radio" id="num10" name="rating" value="10" on:change={onChange} checked={selected===10} />
-    <label for="num10">10</label>
-  </li>
-</ul>
+    {#each Array.from({ length: 10 }, (_, i) => i + 1) as num}
+      <li>
+        <input
+          type="radio"
+          id={`num${num}`}
+          name="rating"
+          value={num}
+          on:change={onChange}
+          checked={selected === num}
+        />
+        <label for={`num${num}`}>{num}</label>
+      </li>
+    {/each}
+  </ul>
 
 <style>
   .rating {
